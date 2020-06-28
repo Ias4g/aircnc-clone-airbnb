a746ff521c20ba2271b6556b14d68068f0500601
Izael silva <izaell.universitario@outlook.com>
2020-06-28 19:26:13 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const multer = require('multer')
+const uploadConfig = require('./config/upload')
+
+const SessionController = require('./controllers/SessionController')
+const SpotsController = require('./controllers/SpotsController')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
+const BookingControler = require('./controllers/BookingController')
 
 const routes = express.Router()
+const upload = multer(uploadConfig)
 
-routes.post('/users', (req, res) => {
-    return res.json(req.body)
-})
+routes.post('/sessions', SessionController.store)
+
+routes.get('/spots', SpotsController.index)
+routes.post('/spots', upload.single('thumbnail'), SpotsController.store)
+
+routes.get('/dashboard', DashboardController.show)
+
+routes.post('/spots/:spot_id/bookings', BookingControler.store)
 
 module.exports = routes
