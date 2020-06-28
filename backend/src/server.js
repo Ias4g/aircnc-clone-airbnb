f2af01db6e04f2aadfdb8d0f11d3aca69e5e15ca
Izael silva <izaell.universitario@outlook.com>
2020-06-28 16:10:13 -0300

    Adding files and routs in project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.json({ nome: "Izael", sobrenome: "alves da silva", idade: "27 anos", estadocivil: "solteiro" })
-})
+// GET, POST, PUT E DELETE
+// req.query - acessa query params (para filtrar)
+// req.params - acessa route params (para adição e deleção)
+// req.body - acessa o corpo da requisição (para criação e edição)
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
     console.log("Back-end running in http://localhost:3333/")
