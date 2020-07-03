b1e10d7998220acb359031dfc9cba6a97cea8528
Izael silva <izaell.universitario@outlook.com>
2020-07-03 16:03:40 -0300

    Importing and configuring CORS dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+
 const routes = require('./routes')
 
 const app = express()
@@ -13,6 +15,7 @@ mongoose.connect('mongodb+srv://aircnc:aircnc@aircnc-euehg.mongodb.net/aircnc?re
 // req.params - acessa route params (para adição e deleção)
 // req.body - acessa o corpo da requisição (para criação e edição)
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
